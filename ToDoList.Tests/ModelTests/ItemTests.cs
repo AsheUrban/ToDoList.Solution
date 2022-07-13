11418f520d992e1f8f0ac8b5cdcce058f6ed693a
Ashe Urban <ashe@goldentongue.com>
2022-07-13 11:34:30 -0700

    ADD all project files and PASS initial item test.
    Co-authored-by: Grace Kostanich <fl5812597@gmail.com>

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTests
+  {
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+    
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string result = newItem.Description;
+
+      //Assert
+      Asssert.AreEqual(description, result);
+    }
+
+  }
+}
