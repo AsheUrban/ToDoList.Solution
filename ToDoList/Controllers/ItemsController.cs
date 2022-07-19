f59797d37024e067685bac5e2a86a9fbccd86055
Ashe Urban <ashe@goldentongue.com>
2022-07-19 14:25:30 -0700

    Add functionality for ClearAll, Show, and update CreateForm to Show.
    correction, not Show, New***
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -15,7 +15,7 @@ namespace ToDoList.Controllers
     }
 
     [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+    public ActionResult New()
     {
       return View();
     }
@@ -26,6 +26,19 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
     
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll ()
+    {
+      Item.ClearAll();
+      return View();
+    }
+
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item foundItem = Item.Find(id);
+      return View(foundItem);
+    }
 
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,12 +6,14 @@ namespace ToDoList.Models
   {
 
     public string Description { get; set; }
+    public int Id { get; }
     private static List<Item> _instances = new List<Item> {};
 
     public Item(string description)
     {
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public static List<Item> GetAll()
@@ -22,6 +24,11 @@ namespace ToDoList.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId - 1];
     }
 
   }
