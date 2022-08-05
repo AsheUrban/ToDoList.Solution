ae69df3f4f6e8ca16e5fbd521ae8791fc9e23500
Ashe Urban <ashe@goldentongue.com>
2022-08-05 14:38:35 -0700

    WIP something.

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -47,8 +47,4 @@ namespace ToDoList
       });
     }
   }
-  public static class DBConfiguration
-  {
-    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
-  }
 }
