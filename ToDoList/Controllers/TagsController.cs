e6ced65ef0b39360d92b92a1ec57d8cbf1f00a70
Ashe Urban <ashe@goldentongue.com>
2023-03-13 12:03:00 -0700

    Add read, create, details for item tag views and controllers.
    Co-authored-by: Sarah Andyshak <sarah.andyshak@gmail.com>

--- ToDoList/Controllers/TagsController.cs
+++ ToDoList/Controllers/TagsController.cs
@@ -28,5 +28,39 @@ namespace ToDoList.Controllers
           .FirstOrDefault(tag => tag.TagId == id);
       return View(thisTag);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Tag tag)
+    {
+      _db.Tags.Add(tag);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddItem(int id)
+    {
+      Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+      ViewBag.ItemId = new SelectList(_db.Items, "ItemId", "Description");
+      return View(thisTag);
+    }
+
+    [HttpPost]
+    public ActionResult AddItem(Tag tag, int itemId)
+    {
+      #nullable enable
+      ItemTag? joinEntity = _db.ItemTags.FirstOrDefault(join => (join.ItemId == itemId && join.TagId == tag.TagId));
+      #nullable disable
+      if(joinEntity == null && itemId !=0)
+      {
+        _db.ItemTags.Add(new ItemTag() { ItemId = itemId, TagId = tag.TagId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = tag.TagId});
+    }
   }
 }
