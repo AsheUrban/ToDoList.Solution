06b4dc7009c10b4620220edec9a98e93c04f6b14
Ashe Urban <ashe@goldentongue.com>
2023-03-14 09:43:30 -0700

    Add data annotations for item class.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,12 +1,18 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.Models
 {
   public class Item
   {
     public int ItemId { get; set; }
+
+    [Required(ErrorMessage="The item's description cannot be empty. Please add a description.")]
     public string Description { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage="You must add your item to a category. If no categories have been created, please add one now.")]
     public int CategoryId { get; set; }
+
     public Category Category { get; set; }
     public List<ItemTag> JoinEntities { get; }
   }
